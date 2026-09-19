86e1e246ff53c681395be19db5fe8180d7e45202
sayeed2758 <sayeedbackupmail@gmail.com>
2026-09-19 07:23:47 +0530

    Add files via upload

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,1 +1,15 @@
-
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+android {
+    namespace = "com.studysathi.app"
+    compileSdk = 36
+    defaultConfig {
+        applicationId = "com.studysathi.app"
+        minSdk = 26
+        targetSdk = 36
+        versionCode = 2
+        versionName = "0.2"
+    }
+}
